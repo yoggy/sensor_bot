32a9f4b722bc54c0f0d303b79692aaf72e26d479
yoggy <yoggy0@gmail.com>
2015-09-13 17:11:09 +0900

    modified:   gruff_cds.rb

--- gruff_cds.rb
+++ gruff_cds.rb
@@ -32,23 +32,23 @@ usage if ARGV.size == 0
 series_name = ARGV[0]
 column_name = "cds"
 
-query_str = "select time, mean(#{column_name}) as #{column_name} from #{series_name} group by time(10m) where time > now() - 3d order asc"
+query_str = "select time, max(#{column_name}) as #{column_name} from #{series_name} group by time(10m) where time > now() - 3d order asc"
 
 gyazo = Gyazo::Client.new
 
 #
 # draw temperature line chart
 #
-g = Gruff::Line.new("640x240")
+g = Gruff::Line.new("640x360")
 g.title = "series:" + series_name
 g.title_font_size = 20
 g.hide_dots = true
 g.line_width = 3
 g.marker_font_size = 14
 g.legend_font_size = 14
-g.minimum_value = 0
-g.maximum_value = 1023
-g.y_axis_increment = 200
+#g.minimum_value = 0
+#g.maximum_value = 1023
+#g.y_axis_increment = 200
 g.theme = {
 	:colors =>  %w(#ff8800),
 	:font_color => 'black',
